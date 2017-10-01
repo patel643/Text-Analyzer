c99ab3620c68b13cf9077f73322c362db0e5165f
Nidhi <31937878+patel643@users.noreply.github.com>
2017-10-01 17:16:05 -0500

    words and graphs

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,24 @@ router.get('/', function(req, res, next) {
 router.post('/aurl', function(req, res, next) {
   var text = req.body.textdata;
 	console.log(text);
-    var nos = text.split('.');
+	
+    var nos = text.split(/[.,?]/); //number of sentences
     console.log(nos.length-1);
-  res.render('aurl',{title: 'Analyzed' , nos:nos.length-1});
+	
+	var wordarr=[];
+	var temp=nos[0].split(" ");
+	wordarr[0]=temp.length;
+	console.log("word 1:"+wordarr[0]);
+	
+	for(var i=1; i<nos.length-1; i++){
+		console.log(nos[i]);
+		temp=nos[i].split(" ");
+		wordarr[i]=temp.length-1;
+		
+		console.log("word "+i+": "+wordarr[i]);
+	}
+	
+  res.render('aurl',{title: 'Analyzed' , nos:nos.length-1, wordarr:wordarr});
 });
 
 module.exports = router;
