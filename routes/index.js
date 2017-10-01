6b6b383762e91078a2a842eb61a601c0732b3a26
Nidhi <31937878+patel643@users.noreply.github.com>
2017-10-01 12:24:14 -0500

    third commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'YouTube Analyzer' });
+  res.render('index', { title: 'Textalyze' });
 });
-
+router.post('/aurl', function(req, res, next) {
+  
+  res.render('aurl',{title: 'Analyzed'});
+});
 module.exports = router;
